7261259320159c821eb768d92d1de4e26a952e11
Slaheddine BEJAOUI <codingsly@gmail.com>
2019-10-26 21:13:51 +0200

    Update SafeWindJammersRunner.py

--- games/windjammers/runners/SafeWindJammersRunner.py
+++ games/windjammers/runners/SafeWindJammersRunner.py
@@ -1,7 +1,7 @@
 import os
 from time import sleep
 
-os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"  # see issue #152
+os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
 os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
 
 from environments import Agent
